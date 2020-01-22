153c21b68c29c89c3eb779cc2a3e4096479c6a3e
yiiouo <15625979610@163.com>
2020-01-22 18:49:41 +0800

    fix: 优化 controller 判断

--- lib/wrapper.ts
+++ lib/wrapper.ts
@@ -1,10 +1,10 @@
 import { Application, Controller, Router } from 'egg';
 import * as _ from 'lodash';
-import {apiObjects} from './decorator';
+import { apiObjects } from './decorator';
 import swaggerHTML from './swaggerHTML';
 import swaggerJSON from './swaggerJSON';
-import {WrapperOptions} from './swaggerJSON';
-import {convertPath, getPath, loadSwaggerClassesToContext} from './utils';
+import { WrapperOptions } from './swaggerJSON';
+import { convertPath, getPath, loadSwaggerClassesToContext } from './utils';
 import validate from './validate';
 /**
  * allowed http methods
@@ -19,38 +19,38 @@ interface Parameters {
 }
 
 const validator = (parameters: Parameters) => async(ctx, next) => {
-  if (!parameters) {
+    if (!parameters) {
+      await next();
+      return;
+    }
+
+    if (parameters.query) {
+      ctx.validatedQuery = validate(ctx.request.query, parameters.query);
+    }
+    if (parameters.path) {
+      ctx.validatedParams = validate(ctx.params, parameters.path);
+    }
+    if (parameters.body) {
+      ctx.validatedBody = validate(ctx.request.body, parameters.body);
+    }
     await next();
-    return;
-  }
+  },
 
-  if (parameters.query) {
-    ctx.validatedQuery = validate(ctx.request.query, parameters.query);
-  }
-  if (parameters.path) {
-    ctx.validatedParams = validate(ctx.params, parameters.path);
-  }
-  if (parameters.body) {
-    ctx.validatedBody = validate(ctx.request.body, parameters.body);
-  }
-  await next();
-};
+  handleSwagger = (router: Router, options: WrapperOptions) => {
+    const {
+      swaggerJsonEndpoint = '/swagger-json',
+      swaggerHtmlEndpoint = '/swagger-html',
+      prefix = ''
+    } = options;
 
-const handleSwagger = (router : Router, options: WrapperOptions) => {
-  const {
-    swaggerJsonEndpoint = '/swagger-json',
-    swaggerHtmlEndpoint = '/swagger-html',
-    prefix = ''
-  } = options;
-
-  // setup swagger router
-  router.get(swaggerJsonEndpoint, async(ctx) => {
-    ctx.body = swaggerJSON(options, apiObjects);
-  });
-  router.get(swaggerHtmlEndpoint, async(ctx) => {
-    ctx.body = swaggerHTML(getPath(prefix, swaggerJsonEndpoint));
-  });
-};
+    // setup swagger router
+    router.get(swaggerJsonEndpoint, async ctx => {
+      ctx.body = swaggerJSON(options, apiObjects);
+    });
+    router.get(swaggerHtmlEndpoint, async ctx => {
+      ctx.body = swaggerHTML(getPath(prefix, swaggerJsonEndpoint));
+    });
+  };
 
 declare module 'egg' {
   interface Application {
@@ -64,77 +64,81 @@ declare module 'egg' {
   }
 }
 
-const handleMap = (app : Application, ControllerClass : typeof Controller) => {
-  const anonymousContext = app.createAnonymousContext();
-  const router = app.router;
-  const c : Controller = new ControllerClass(Object.assign(anonymousContext));
-  const methods : string[] = Object.getOwnPropertyNames(Object.getPrototypeOf(c));
-  // remove useless field in class object:  constructor, length, name, prototype
-  _.pull(methods, 'name', 'constructor', 'length', 'prototype', 'pathName', 'fullPath');
-  // map all method in methods
-  methods
-  // filter methods without @request decorator
-    .filter((item) => {
-    const {path, method} = c[item];
-    if (!path && !method) {
-      return false;
-    }
-    return true;
-  })
-  // add router
-    .forEach((item) => {
-    const {path, method} = c[item];
-    let {
-      middlewares = []
-    } = c[item];
-    if (typeof middlewares === 'function') {
-      middlewares = [middlewares];
-    }
-    if (!Array.isArray(middlewares)) {
-      throw new Error('middlewares params must be an array or function');
-    }
-    middlewares.forEach((item) => {
-      if (typeof item !== 'function') {
-        throw new Error('item in middlewares must be a function');
-      }
+const handleMap = (app: Application, ControllerClass: typeof Controller) => {
+    const anonymousContext = app.createAnonymousContext(),
+      router = app.router,
+      c: Controller = new ControllerClass(Object.assign(anonymousContext)),
+      methods: string[] = Object.getOwnPropertyNames(Object.getPrototypeOf(c));
+    // remove useless field in class object:  constructor, length, name, prototype
+    _.pull(methods, 'name', 'constructor', 'length', 'prototype', 'pathName', 'fullPath');
+    // map all method in methods
+    methods
+    // filter methods without @request decorator
+      .filter(item => {
+        const { path, method } = c[item];
+        if (!path && !method) {
+          return false;
+        }
+        return true;
+      })
+    // add router
+      .forEach(item => {
+        const { path, method } = c[item];
+        let {
+          middlewares = []
+        } = c[item];
+        if (typeof middlewares === 'function') {
+          middlewares = [middlewares];
+        }
+        if (!Array.isArray(middlewares)) {
+          throw new Error('middlewares params must be an array or function');
+        }
+        middlewares.forEach(item => {
+          if (typeof item !== 'function') {
+            throw new Error('item in middlewares must be a function');
+          }
+        });
+        if (!reqMethods.includes(method)) {
+          throw new Error(`illegal API: ${method} ${path} at [${item}]`);
+        }
+        const chain = [
+          `${convertPath(path)}`,
+          validator(c[item].parameters),
+          ...middlewares,
+          async ctx => {
+            const c = new ControllerClass(ctx);
+            await c[item]();
+          }
+        ];
+        router[method](...chain);
+      });
+  },
+
+  handleMapDir = (app: Application) => {
+    loadSwaggerClassesToContext(app);
+    const classes = app.swaggerControllerClasses;
+    Object.keys(classes).forEach(name => {
+      const controller = classes[name].index || classes[name];
+      if (!controller || typeof controller !== 'function') return false;
+      handleMap(app, controller);
     });
-    if (!reqMethods.includes(method)) {
-      throw new Error(`illegal API: ${method} ${path} at [${item}]`);
-    }
-    const chain = [
-      `${convertPath(path)}`,
-      validator(c[item].parameters),
-      ...middlewares,
-      async (ctx) => {
-        const c = new ControllerClass(ctx);
-        await c[item]();
-      }
-    ];
-    router[method](...chain);
-  });
-};
+  },
 
-const handleMapDir = (app: Application) => {
-  loadSwaggerClassesToContext(app);
-  const classes = app.swaggerControllerClasses;
-  Object.keys(classes).forEach((name) => {handleMap(app, classes[name]);});
-};
+  wrapper = (app: Application, options?: WrapperOptions) => {
+    const opts: WrapperOptions = {
+      title: 'API DOC',
+      description: 'API DOC',
+      version: 'v1.0.0',
+      prefix: '',
+      swaggerJsonEndpoint: '/swagger-json',
+      swaggerHtmlEndpoint: '/swagger-html',
+      makeSwaggerRouter: false,
+    };
+    Object.assign(opts, options || {});
 
-const wrapper = (app : Application, options?: WrapperOptions) => {
-  const opts: WrapperOptions = {
-    title: 'API DOC',
-    description: 'API DOC',
-    version: 'v1.0.0',
-    prefix: '',
-    swaggerJsonEndpoint: '/swagger-json',
-    swaggerHtmlEndpoint: '/swagger-html',
-    makeSwaggerRouter: false,
-  };
-  Object.assign(opts, options || {});
-
-  const {router} = app;
-  if (makeSwaggerRouter) {handleMapDir(app); }
-  handleSwagger(router, opts);
-};
-const makeSwaggerRouter = (app: Application) => handleMapDir(app);
-export  {wrapper, makeSwaggerRouter};
+    const { router } = app;
+    if (makeSwaggerRouter) { handleMapDir(app); }
+    handleSwagger(router, opts);
+  },
+  makeSwaggerRouter = (app: Application) => handleMapDir(app);
+export { wrapper, makeSwaggerRouter };
